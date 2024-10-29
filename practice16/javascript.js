ba33be637043c287c1fd790397e6bb4503474521
中畠昇汰 <pandora.osk@gmail.com>
2024-10-29 15:16:26 +0800

    practice16

---
+++ practice16/javascript.js
@@ -0,0 +1,18 @@
+for (let i = 1; i <= 100; i++) {
+    let output = i;
+
+    // 条件に応じて文字列を追加
+    if (i % 3 === 0) {
+        output += "a";
+    }
+    if (i % 5 === 0) {
+        output += "b";
+    }
+    if (i % 15 === 0) {
+        output += "c";
+    }
+
+    console.log(output);
+}
+
+
