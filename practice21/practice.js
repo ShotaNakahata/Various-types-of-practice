4e2b386b11aa98548eeec555d9ca192aa502cc1c
中畠昇汰 <pandora.osk@gmail.com>
2024-10-31 20:01:28 +0800

    practice21_v3

--- practice21/practice.js
+++ practice21/practice.js
@@ -5,11 +5,11 @@
 // 次のような配列 numbers が与えられています。この配列のすべての数値の合計を計算する関数 sumArray(numbers) を実装してください。
 const numbers = [10, 20, 30, 40];
 
-function sumArray(numbers){
-    const sum =numbers.reduce((acc,number)=>{
-        acc +=number
+function sumArray(numbers) {
+    const sum = numbers.reduce((acc, number) => {
+        acc += number
         return acc
-    },0)
+    }, 0)
     console.log(sum)
 }
 
@@ -27,14 +27,37 @@ const fruits = ["apple", "banana", "orange", "apple", "banana", "apple"];
 
 function countFruits(fruits) {
     const countFruits = {}
-    const output =fruits.reduce((acc,frut)=>{
-        if(acc[frut]){
-            acc[frut]+=1
-        }else{
+    const output = fruits.reduce((acc, frut) => {
+        if (acc[frut]) {
+            acc[frut] += 1
+        } else {
             acc[frut] = 1
         }
         return acc
-    },countFruits)
+    }, countFruits)
     console.log(output);
 }
 countFruits(fruits); // 出力例: { apple: 3, banana: 2, orange: 1 }
+
+// 問題3: 配列内のオブジェクトの特定プロパティを集計
+// 目的: 配列内のオブジェクトのデータを集計し、特定のプロパティの合計を取得する方法を学びます。
+
+// 課題内容
+// ショッピングカートに入っている商品リストが配列cartとして与えられています。
+// 各商品オブジェクトには、商品名 name と price プロパティが含まれています。
+// カート内の商品の合計価格を計算する関数 calculateTotal(cart) を実装してください
+
+const cart = [
+    { name: "Apple", price: 120 },
+    { name: "Banana", price: 80 },
+    { name: "Orange", price: 100 }
+];
+
+function calculateTotal(cart) {
+    const output = cart.reduce((acc, product) => {
+        acc += product.price
+        return acc
+    }, 0)
+    console.log(output)
+}
+calculateTotal(cart); // 300
