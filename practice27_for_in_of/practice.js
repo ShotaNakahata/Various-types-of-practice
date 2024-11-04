bfbe972f471db6859c82bbb7bd675addacbc39f9
中畠昇汰 <pandora.osk@gmail.com>
2024-11-05 05:27:48 +0800

    practice27_for_in_of

---
+++ practice27_for_in_of/practice.js
@@ -0,0 +1,24 @@
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+// 1. 基本編：オブジェクトのキーと値のループ（for...in）
+// 目的: for...inを使って、オブジェクトのキーとその値をループ処理します。
+
+// 問題
+// 次のオブジェクトuserから、キーと値を取り出して「キー: 値」の形式で出力してください。
+const user = {
+    name: "Alice",
+    age: 25,
+    city: "New York"
+};
+
+function outputKeyAndValue(user){
+    for(let key in user){
+        console.log(key,":",user[key])
+    }
+}
+outputKeyAndValue(user)
+
+// output
+// name: Alice
+// age: 25
+// city: New York
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
