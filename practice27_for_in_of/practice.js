eb722816d0aa52d7a5ef567302b3d3c2697cc5b4
中畠昇汰 <pandora.osk@gmail.com>
2024-11-05 05:31:33 +0800

    practice27_for_in_of_2

--- practice27_for_in_of/practice.js
+++ practice27_for_in_of/practice.js
@@ -4,21 +4,38 @@
 
 // 問題
 // 次のオブジェクトuserから、キーと値を取り出して「キー: 値」の形式で出力してください。
-const user = {
-    name: "Alice",
-    age: 25,
-    city: "New York"
-};
+// const user = {
+//     name: "Alice",
+//     age: 25,
+//     city: "New York"
+// };
 
-function outputKeyAndValue(user){
-    for(let key in user){
-        console.log(`${key}:${user[key]}`)
-    }
-}
-outputKeyAndValue(user)
+// function outputKeyAndValue(user){
+//     for(let key in user){
+//         console.log(`${key}:${user[key]}`)
+//     }
+// }
+// outputKeyAndValue(user)
 
 // output
 // name: Alice
 // age: 25
 // city: New York
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+// 問題 2：配列のループ（for...of）
+// 目的: for...ofを使って、配列の各要素をループ処理し、出力する方法を学びます。
+
+// 課題
+// 次の配列colorsに含まれる色の名前を1つずつ出力してください。
+const colors = ["red", "green", "blue"];
+for (let color of colors){
+    console.log(color)
+}
+// [output]
+// red
+// green
+// blue
+
+
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
