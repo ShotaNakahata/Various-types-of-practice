3cfe11121a0b5f710493d54e12511b4f166651ec
中畠昇汰 <pandora.osk@gmail.com>
2024-11-08 07:49:18 +0800

    practice29_Promise_1

--- practice29_Promise/practice.js
+++ practice29_Promise/practice.js
@@ -4,6 +4,19 @@
 // 課題
 // 以下のコードを完成させてください。delayedHello 関数は、2 秒後に "Hello, World!" 
 // というメッセージを console.log に出力します。Promise を使用して非同期処理を実装してください。
+function delayedHello() {
+    // Promise を作成し、2 秒後に "Hello, World!" を出力するように実装してください。
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve("Hello, World!")
+        }, 2000);
+    })
+}
+
+delayedHello().then(message=>{
+    console.log(message)
+});
+
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
