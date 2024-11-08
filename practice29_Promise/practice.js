74a4dfd33bfe2e9b112ababa1e9aaf23b79ef9c3
中畠昇汰 <pandora.osk@gmail.com>
2024-11-08 08:17:08 +0800

    practice29_Promise_3

--- practice29_Promise/practice.js
+++ practice29_Promise/practice.js
@@ -23,21 +23,64 @@
 // 課題
 // 以下の関数 fetchData は、2 秒後にデータを返す Promise を返します。
 // Promise チェーンを使用して、この関数を呼び出し、2 回連続でデータを取得し、その結果を出力してください。
-function fetchData(){
-    return new Promise((resolve,reject)=>{
+// function fetchData(){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(() => {
+//             resolve("Data received")
+//         }, 2000);
+//     })
+// }
+// fetchData().then(result=>{
+//     console.log(result);
+//     return fetchData()
+// }).then(result=>{
+//     console.log(result)
+// })
+
+// fetchData を2回呼び出して、それぞれの結果を出力するPromiseチェーンを作成してください。
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+// 課題
+// 以下に 3 つの非同期関数 fetchFirst, fetchSecond, fetchThird があります。
+// それぞれが 1 秒、2 秒、3 秒の遅延の後に異なるデータを返します。これらを順番に実行し、
+// 全ての結果を一つの配列にまとめて最後に console.log() で出力してください。
+function fetchFirst(){
+    return new Promise((resolve,rejecet)=>{
         setTimeout(() => {
-            resolve("Data received")
+            resolve("First result")
+        }, 1000);
+    })
+}
+function fetchSecond(){
+    return new Promise((resolve,rejecet)=>{
+        setTimeout(() => {
+            resolve("Second result")
         }, 2000);
     })
 }
-fetchData().then(result=>{
-    console.log(result);
-    return fetchData()
+function fetchThird(){
+    return new Promise((resolve,rejecet)=>{
+        setTimeout(() => {
+            resolve("Third result")
+        }, 3000);
+    })
+}
+let output = []
+fetchFirst().then(result=>{
+    output.push(result)
+    return fetchSecond()
 }).then(result=>{
-    console.log(result)
+    output.push(result)
+    return fetchThird()
+}).then(result=>{
+    output.push(result)
+    console.log(output)
 })
 
-// fetchData を2回呼び出して、それぞれの結果を出力するPromiseチェーンを作成してください。
 
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
