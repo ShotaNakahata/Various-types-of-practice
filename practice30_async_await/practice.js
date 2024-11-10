8b8b588c05068fde1d362fa11f63e3b231bf107f
中畠昇汰 <pandora.osk@gmail.com>
2024-11-10 12:14:24 +0800

    practice30_async_await_14

--- practice30_async_await/practice.js
+++ practice30_async_await/practice.js
@@ -573,8 +573,12 @@
 // 適切なエラーハンドリングを実装してください。
 async function fetchAndMergeData() {
     try {
-        const userResponse = await fetch("https://jsonplaceholder.typicode.com/users");
-        const postResponse = await fetch("https://jsonplaceholder.typicode.com/posts");
+        // const userResponse = await fetch("https://jsonplaceholder.typicode.com/users");
+        // const postResponse = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const [userResponse,postResponse] = await Promise.all([
+            fetch("https://jsonplaceholder.typicode.com/users"),
+            fetch("https://jsonplaceholder.typicode.com/posts")
+        ]);
         if (!userResponse.ok || !postResponse.ok) {
             throw new Error("fetch result is not ok");
         }
