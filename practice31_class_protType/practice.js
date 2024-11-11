b02ea82af617cabcc0461de23926653530f53410
中畠昇汰 <pandora.osk@gmail.com>
2024-11-11 10:12:22 +0800

    practice31_class_protType 5

--- practice31_class_protType/practice.js
+++ practice31_class_protType/practice.js
@@ -28,28 +28,71 @@
 // このメソッドは"${name} is studying for a ${grade} grade."というメッセージをconsole.logで出力します。
 // introduce()メソッドはPersonクラスから継承し、superを使って親クラスのintroduce()メソッドを呼び出してください。
 
+// class Person {
+//     constructor(name, age) {
+//         this.name = name
+//         this.age = age
+//     }
+//     introduce() {
+//         console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
+//     }
+// }
+// class Student extends Person{
+//     constructor(name,grade,age){
+//         super(name,age);
+//         this.grade=grade
+//     }
+//     study(){
+//         console.log(`${this.name} is studying for a ${this.grade} grade.`)
+//     }
+// }
+// const student = new Student("Alice",3,20)
+// student.study();
+// student.introduce();
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+// 応用問題 2: 多段階の継承
+// 以下の要件に従って、新しいクラスを作成してください。
+
+// 要件:
+
+// すでにあるPersonクラスとStudentクラスを基に、新しいGraduateStudentクラスを作成してください。
+// GraduateStudentクラスはStudentクラスを継承し、新しいプロパティthesisTitle（論文のタイトル）を追加します。
+// GraduateStudentクラスにdefendThesis()というメソッドを追加してください。
+// このメソッドは、"${name} is defending their thesis titled '${thesisTitle}'."
+// というメッセージをconsole.logで出力します。
 class Person {
     constructor(name, age) {
-        this.name = name
+        this.name = name;
         this.age = age
     }
     introduce() {
         console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
     }
 }
-class Student extends Person{
-    constructor(name,grade,age){
-        super(name,age);
-        this.grade=grade
+
+class Student extends Person {
+    constructor(name, age, grade) {
+        super(name, age);
+        this.grade = grade;
     }
-    study(){
+    study() {
         console.log(`${this.name} is studying for a ${this.grade} grade.`)
     }
 }
-const student = new Student("Alice",3,20)
-student.study();
-student.introduce();
-// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+class GraduateStudent extends Student {
+    constructor(name, age, grade, thesisTitle) {
+        super(name, age, grade);
+        this.thesisTitle = thesisTitle
+    }
+    defendThesis() {
+        console.log(`${this.name} is defending their thesis titled '${this.thesisTitle}'.`)
+    }
+}
+const GraduateStudent1 = new GraduateStudent("Alice", 24, "A", "Quantum Physics")
+GraduateStudent1.defendThesis();
+// student.study();
+// student.introduce();
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
