035ade1c96fe29450bc326a8648b509dd89f6e30
中畠昇汰 <pandora.osk@gmail.com>
2024-11-11 09:54:09 +0800

    practice31_class_protType 3

--- practice31_class_protType/practice.js
+++ practice31_class_protType/practice.js
@@ -17,7 +17,37 @@
 // const person1 = new Person("Alice",23)
 // person1.introduce()
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+// 応用問題 1: クラスの継承
+// 次の要件に従って、新しいクラスを作成してください。
 
+// 要件:
+
+// Personクラスを基に、新しいStudentクラスを作成してください。
+// StudentクラスはPersonクラスを継承し、新しいプロパティとしてgrade（成績）を追加します。
+// Studentクラスに、study()というメソッドを追加してください。
+// このメソッドは"${name} is studying for a ${grade} grade."というメッセージをconsole.logで出力します。
+// introduce()メソッドはPersonクラスから継承し、superを使って親クラスのintroduce()メソッドを呼び出してください。
+
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+    introduce() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
+    }
+}
+class Student extends Person{
+    constructor(name,grade){
+        super(name);
+        this.grade=grade
+    }
+    study(){
+        console.log(`${this.name} is studying for a ${this.grade} grade.`)
+    }
+}
+const student = new Student("Alice",3)
+student.study();
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
