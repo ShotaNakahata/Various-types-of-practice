46a67cbd8a687f3bd1ae5c0a8a1301744db69bdc
中畠昇汰 <pandora.osk@gmail.com>
2024-11-11 08:56:38 +0800

    practice31_class_protType 2

--- practice31_class_protType/practice.js
+++ practice31_class_protType/practice.js
@@ -4,20 +4,20 @@
 // コンストラクタにname（名前）とage（年齢）を引数として取り、インスタンスのプロパティとして設定します。
 // introduceというメソッドを持ち、このメソッドは"Hello, my name is [name] and I am [age] years old."
 // という文字列を返します。
-class Person{
-    constructor(name,age){
-        this.name = name
-        this.age = age
-    }
-    introduce(){
-        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
-    }
-}
+// class Person{
+//     constructor(name,age){
+//         this.name = name
+//         this.age = age
+//     }
+//     introduce(){
+//         console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
+//     }
+// }
 
-
-
-
+// const person1 = new Person("Alice",23)
+// person1.introduce()
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
