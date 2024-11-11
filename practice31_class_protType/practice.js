8f0ec3dd9871ca04d3b5d76fe387bc60d9f3116e
中畠昇汰 <pandora.osk@gmail.com>
2024-11-11 16:09:05 +0800

    practice31_class_protType 6

--- practice31_class_protType/practice.js
+++ practice31_class_protType/practice.js
@@ -60,39 +60,144 @@
 // GraduateStudentクラスにdefendThesis()というメソッドを追加してください。
 // このメソッドは、"${name} is defending their thesis titled '${thesisTitle}'."
 // というメッセージをconsole.logで出力します。
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age
-    }
-    introduce() {
-        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
-    }
-}
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age
+//     }
+//     introduce() {
+//         console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
+//     }
+// }
 
-class Student extends Person {
-    constructor(name, age, grade) {
-        super(name, age);
-        this.grade = grade;
-    }
-    study() {
-        console.log(`${this.name} is studying for a ${this.grade} grade.`)
-    }
-}
+// class Student extends Person {
+//     constructor(name, age, grade) {
+//         super(name, age);
+//         this.grade = grade;
+//     }
+//     study() {
+//         console.log(`${this.name} is studying for a ${this.grade} grade.`)
+//     }
+// }
 
-class GraduateStudent extends Student {
-    constructor(name, age, grade, thesisTitle) {
-        super(name, age, grade);
-        this.thesisTitle = thesisTitle
-    }
-    defendThesis() {
-        console.log(`${this.name} is defending their thesis titled '${this.thesisTitle}'.`)
-    }
-}
-const GraduateStudent1 = new GraduateStudent("Alice", 24, "A", "Quantum Physics")
-GraduateStudent1.defendThesis();
+// class GraduateStudent extends Student {
+//     constructor(name, age, grade, thesisTitle) {
+//         super(name, age, grade);
+//         this.thesisTitle = thesisTitle
+//     }
+//     defendThesis() {
+//         console.log(`${this.name} is defending their thesis titled '${this.thesisTitle}'.`)
+//     }
+// }
+// const GraduateStudent1 = new GraduateStudent("Alice", 24, "A", "Quantum Physics")
+// GraduateStudent1.defendThesis();
 // student.study();
 // student.introduce();
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+// 応用問題 3: メソッドのオーバーライドとsuperの使用
+// 次の要件に従って、新しいクラスを作成し、メソッドのオーバーライドを学びましょう。
+
+// 要件:
+
+// すでにあるPersonクラスとStudentクラスを基に、新しいGraduateStudentクラスを作成してください。
+// GraduateStudentクラスは、Studentクラスのstudy()メソッドをオーバーライドし、
+// 新しいメッセージ"${name} is studying hard for their final thesis titled '${thesisTitle}'."を
+// console.logで出力してください。
+// GraduateStudentクラスのstudy()メソッド内で、親クラスStudentのstudy()メソッドも呼び出して、
+// 元のメッセージも出力されるようにしてください。
+// class Person {
+//     constructor(name, age) {
+//         this.name = name
+//         this.age = age
+//     }
+//     introduce() {
+//         console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
+//     }
+// }
+
+// class Student extends Person {
+//     constructor(name, age, grade) {
+//         super(name, age)
+//         this.grade = grade
+//     }
+//     study() {
+//         console.log(`${this.name} is studying for a ${this.grade} grade.`)
+//     }
+// }
+
+// class GraduateStudent extends Student {
+//     constructor(name, age, grade, thesisTitle) {
+//         super(name, age, grade)
+//         this.thesisTitle = thesisTitle;
+//     }
+//     defendThesis() {
+//         console.log(`${this.name} is defending their thesis titled '${this.thesisTitle}'.`)
+//     }
+//     study(){
+//         console.log(`${this.name} is studying hard for their final thesis titled '${this.thesisTitle}`)
+//     }
+// }
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+// 応用問題 4: クラスと継承を使った多層構造の実装
+// 以下の要件に従って、複数のクラスを実装してください。
+
+// 要件:
+// Animalクラスを作成してください。
+// コンストラクタはnameとspeciesを受け取り、プロパティとして設定します。
+// describe()メソッドを追加し、"${name} is a ${species}."をconsole.logで出力します。
+// MammalクラスをAnimalクラスから継承してください。
+// MammalクラスにはhasFurというプロパティを追加し、コンストラクタで受け取ります。
+// describe()メソッドをオーバーライドし、super.describe()を呼び出した後、
+// "${name} has fur: ${hasFur}."を追加で出力します。
+// DogクラスをMammalクラスから継承し、bark()メソッドを追加してください。
+// bark()メソッドは"${name} barks: Woof!"をconsole.logで出力します。
+
+class Animal{
+    constructor(name,species){
+        this.name=name
+        this.species=species
+    }
+    describe(){
+        console.log(`${this.name} is a ${this.species}.`)
+    }
+}
+class Mammal extends Animal{
+    constructor(name,species,hasFur){
+        super(name,species)
+        this.hasFur=hasFur
+    }
+    describe(){
+        super.describe();
+        console.log(`${this.name} has fur: ${this.hasFur}.`)
+    }
+}
+class Dog extends Mammal{
+    constructor(name,species,hasFur){
+        super(name,species,hasFur);
+    }
+    bark(){
+        console.log(`${this.name} barks: Woof`)
+    }
+}
+
+const animal = new Animal("Generic Animal", "unknown species");
+animal.describe(); // "Generic Animal is a unknown species."
+
+const mammal = new Mammal("Elephant", "Elephas maximus", true);
+mammal.describe();
+// 出力:
+// "Elephant is a Elephas maximus."
+// "Elephant has fur: true."
+
+const dog = new Dog("Buddy", "Canis lupus familiaris", true);
+dog.describe();
+// 出力:
+// "Buddy is a Canis lupus familiaris."
+// "Buddy has fur: true."
+dog.bark(); // "Buddy barks: Woof!"
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
