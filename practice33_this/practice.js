90968cf7dd577734e51853cc3fb6c1cfdab98ac5
中畠昇汰 <pandora.osk@gmail.com>
2024-11-13 10:24:09 +0800

    practice33_this 2

--- practice33_this/practice.js
+++ practice33_this/practice.js
@@ -2,16 +2,28 @@
 // 要件
 // 関数describeはthisを使用してオブジェクトの情報を出力します。
 // callメソッドを使って、オブジェクトpersonをthisとして指定し、正しい出力を得てください。
+// function describe(age, city) {
+//     console.log(`I am ${this.name}, ${age} years old, and I live in ${city}.`);
+// }
+
+// const person = { name: 'John' };
+// describe.call(person,25,"Tokyo")
+// describeを使って正しい出力を得ること
+// 出力例: "I am John, 25 years old, and I live in Tokyo."
+
+// ------------------------------------------------------------------------------
+// 問題 2: applyメソッドを使う
+// 要件
+// 同じdescribe関数を使います。
+// 今度は**applyメソッド**を使って、同じ出力を得てください。
+// 引数は配列として渡す必要があります。
 function describe(age, city) {
     console.log(`I am ${this.name}, ${age} years old, and I live in ${city}.`);
 }
 
 const person = { name: 'John' };
-describe.call(person,25,"Tokyo")
-// describeを使って正しい出力を得ること
-// 出力例: "I am John, 25 years old, and I live in Tokyo."
-
-// ------------------------------------------------------------------------------
+// describe.call(person, 25, "Tokyo");
+describe.apply(person,[25,"Tokyo"])
 // ------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------
