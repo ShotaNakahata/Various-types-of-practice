80fcfaac1519b63f6eadc69a524a316fdfbef45f
中畠昇汰 <pandora.osk@gmail.com>
2024-11-22 05:53:48 +0800

    Udemy_fromPython_algorithm firstcommit_8

--- Udemy_fromPython_algorithm/practice1.js
+++ Udemy_fromPython_algorithm/practice1.js
@@ -42,44 +42,58 @@
 // OnO([1,2,3,4,5])
 //-------------------------------------------------------
 
-function BubbleSort(numbers) {
-    let count = 0
-    for(let j = 0; j<numbers.length; j++){
-        console.log(j+1,"回目（外側）")
-        count++
-        for (let i = 0; i < numbers.length -j - 1; i++) {
-            if (numbers[i] > numbers[i + 1]) {
-                [numbers[i], numbers[i + 1]] = [numbers[i + 1], numbers[i]]
+// function BubbleSort(numbers) {
+//     let count = 0
+//     for(let j = 0; j<numbers.length; j++){
+//         console.log(j+1,"回目（外側）")
+//         count++
+//         for (let i = 0; i < numbers.length -j - 1; i++) {
+//             if (numbers[i] > numbers[i + 1]) {
+//                 [numbers[i], numbers[i + 1]] = [numbers[i + 1], numbers[i]]
+//             }
+//             console.log(j+1,"回目（内側) ",i,"文字目から ",`${numbers.length -j - 1}`," 文字目まで調べた")
+//             count++
+//         }
+//     }
+//     console.log(`最終回数：${count}回`)
+//     console.log(numbers)
+// }
+// BubbleSort([6, 4, 5, 3, 1, 2])
+//-------------------------------------------------------
+function cocktailsort(numbers) {
+    let count = 1
+    let outsideCount = 1
+    let insideCount = 1
+    let end = numbers.length - 1
+    let start = 0
+    let swap = false;
+    // let derection = ture
+    for (let i = 0; i < numbers.length; i++) {
+        console.log("from 外側 : ", outsideCount, "回目")
+        for (let j = 0; j < numbers.length - 1; j++) {
+            if (numbers[j] > numbers[j + 1]) {
+                [numbers[j], numbers[j + 1]] = [numbers[j + 1], numbers[j]]
+                swap = true
             }
-            console.log(j+1,"回目（内側) ",i,"文字目から ",`${numbers.length -j - 1}`," 文字目まで調べた")
+            console.log("from 内側 : ", insideCount, "回目")
+            insideCount++
             count++
         }
+        if (swap === false){
+            console.log(`最終回数：${count}回 swap is false`)
+            return console.log(numbers)
+            
+        };
+        swap = false
+        // derection = !derection
+        outsideCount++
+        count++
     }
     console.log(`最終回数：${count}回`)
     console.log(numbers)
 }
-BubbleSort([6, 4, 5, 3, 1, 2])
-//-------------------------------------------------------
-// function cocktailsort(numbers) {
-//     let end = numbers.length-1
-//     let start = 0
-//     let swap = false;
-//     let derection = ture
-//     for (let i = 0; i < numbers.length; i++) {
-//         for (let j = 0; j < numbers.length - 1; j++) {
-//             if (numbers[j] > numbers[j + 1]) {
-//                 [numbers[j], numbers[j + 1]] = [numbers[j + 1], numbers[j]]
-//                 swap = ture
-//             }
-//             if(swap!=ture) return console.log(numbers);
-//             swap = false
-//         }
-//         derection = !derection
-//     }
-//     console.log(numbers)
-// }
-// // console.log(cocktailsort([1, 5, 3, 2, 4]))
-// cocktailsort([1, 5, 3, 2, 4])
+// console.log(cocktailsort([1, 5, 3, 2, 4]))
+cocktailsort([1, 5, 3, 2, 4])
 //-------------------------------------------------------
 //-------------------------------------------------------
 //-------------------------------------------------------
