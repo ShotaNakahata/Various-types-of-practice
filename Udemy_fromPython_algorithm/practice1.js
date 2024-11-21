265eaac0a9a81b6a4e2223f7c01338a6e3c02494
中畠昇汰 <pandora.osk@gmail.com>
2024-11-22 03:21:51 +0800

    Udemy_fromPython_algorithm firstcommit_2

--- Udemy_fromPython_algorithm/practice1.js
+++ Udemy_fromPython_algorithm/practice1.js
@@ -1,12 +1,23 @@
 //O(log(n)) O(log10) 
 
-function logn(numbers) {
-    if(numbers<1){
-        return console.log("finished");
-    }else{
-        console.log(numbers)
-        return logn(numbers/2)
-    }
+// function logn(numbers) {
+//     if(numbers<1){
+//         return console.log("finished");
+//     }else{
+//         console.log(numbers)
+//         return logn(numbers/2)
+//     }
     
+// }
+// logn(10)
+//-------------------------------------------------------
+function On(numbers) {
+    for(let number of numbers){
+        console.log(number)
+    }
 }
-logn(10)
+On([1,2,3,4,5])
+//-------------------------------------------------------
+//-------------------------------------------------------
+//-------------------------------------------------------
+//-------------------------------------------------------
